932f7512e2dad8f3ab49f40842ae89a1b42a1384
ElouadiAbdelati <57566321+ElouadiAbdelati@users.noreply.github.com>
2020-09-25 19:02:53 +0200

    Express Session Part 2

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Shema = mongoose.Schema();
+
+const userShema = new Shema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', userShema);
