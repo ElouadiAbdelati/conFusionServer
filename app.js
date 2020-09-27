6810a8abb9747ce9ace861870b5bf65fcd6fd6c0
ElouadiAbdelati <57566321+ElouadiAbdelati@users.noreply.github.com>
2020-09-27 19:27:30 +0200

    https and upload file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
-
+var uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const url = config.mongoUrl;
@@ -27,7 +27,14 @@ connect.then((db) => {
     console.log(err);
 });
 var app = express();
-
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+        return next();
+    } else {
+        res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -35,6 +42,10 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//upload file 
+app.use('/imageUpload', uploadRouter);
+//end 
 
 //start users auth
 
